9094621ee2ee636c6897f6b8e6d2da4ff37583e7
nate421 <greg_julie@roadrunner.com>
2016-10-20 10:44:19 -0700

    turtle

---
+++ turtle.py
@@ -0,0 +1,12 @@
+import turtle
+
+def draw_square():
+    # window = turtle.Screen()
+    # window.rbgcolor("red")
+
+    brad = turtle.Turtle()
+    brad.forward(100)
+
+    window.exitoncilck()
+
+draw_square()
